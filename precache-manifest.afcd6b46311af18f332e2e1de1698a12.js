39db07fcf867c241c6897c4106c56d36f46ef230
shankar55 <shankar511998@gmail.com>
2024-01-08 11:05:09 +0530

    Updates

---
+++ precache-manifest.afcd6b46311af18f332e2e1de1698a12.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8e03bcd8a7a8438db6507d30aa8ab69",
+    "url": "/todolist-react/index.html"
+  },
+  {
+    "revision": "55048e8393d9bbb94773",
+    "url": "/todolist-react/static/js/2.c3e477d3.chunk.js"
+  },
+  {
+    "revision": "5cc73030547f08da4058",
+    "url": "/todolist-react/static/js/main.e77d44cb.chunk.js"
+  },
+  {
+    "revision": "cc24e69ec6a30e8f77b3",
+    "url": "/todolist-react/static/js/runtime-main.0d8f41e2.js"
+  }
+]);
